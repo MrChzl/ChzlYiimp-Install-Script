41c71ded058146cf002b2e4bc63866a394736736
MrChzl <LifeEscalade@gmail.com>
2023-06-25 13:54:53 -0400

    Update install-debug.sh

--- install-debug.sh
+++ install-debug.sh
@@ -115,7 +115,7 @@
     apt-get -y --purge autoremove
     fi
 
-    sudo apt -y install nginx
+    sudo apt-get -y install nginx
     sudo rm /etc/nginx/sites-enabled/default
     sudo systemctl start nginx.service
     sudo systemctl enable nginx.service
@@ -182,7 +182,7 @@
     else
     sudo apt -y install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
     php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt php7.3-memcache\
-    php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring \
+    php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl php7.3-memcached php7.3-memcache php7.3-imagick php7.3-gettext php7.3-zip php7.3-mbstring \
     libpsl-dev libnghttp2-dev
     fi
     sleep 5
